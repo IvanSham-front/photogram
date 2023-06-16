787e865df92e292800815f3afc2fe870488ce66d
Ivan <igshamenkov@sbercloud.ru>
2023-06-16 16:05:58 +0300

    add dynamic callbackurl

--- src/unsplash/unsplash.js
+++ src/unsplash/unsplash.js
@@ -1,10 +1,12 @@
 import Unsplash from "unsplash-js";
 import { accessKey, secretKey } from "./keys";
 
+const callbackUrl = window.location.origin
+
 const unsplashUser = new Unsplash({
     accessKey: accessKey,
     secret: secretKey,
-    callbackUrl: 'http://localhost:3000'
+    callbackUrl
   })
 
 export default unsplashUser
